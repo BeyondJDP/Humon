4dcad5d7a7ab8da7decca54b1529665ac5ae4320
BeyondJDP <joel.vermeulen@hotmail.com>
2020-02-25 13:22:39 +0100

    Initiall comment

---
+++ Assets/Joel/Scripts/OptionsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+      
+
+    private void Start()
+    {
+        
+    }
+
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}

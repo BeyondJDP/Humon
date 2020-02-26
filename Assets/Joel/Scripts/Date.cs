3a9ebea447f1f48f4e8df52d55004a815fd6e9de
BeyondJDP <joel.vermeulen@hotmail.com>
2020-02-26 13:22:19 +0100

    Added back

---
+++ Assets/Joel/Scripts/Date.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Date : MonoBehaviour
+{
+
+    public TextMeshPro largeText;
+
+    void Start()
+    {
+        string time = System.DateTime.UtcNow.ToLocalTime().ToString("dd-MM-yyyy HH:mm");
+        largeText.text = time;
+
+    }
+
+}
